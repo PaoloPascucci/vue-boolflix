afed90df82b7efdebb5fce2345da64dcd0d63e2a
PaoloPascucci <pascuccip9@gmail.com>
2021-11-18 15:52:51 +0100

    Add Film and color

--- src/components/siteMain.vue
+++ src/components/siteMain.vue
@@ -2,14 +2,25 @@
 <div>
     <div class="header">
     <Search :filmSearch="searchText" @search_film="UserInput"/>
+    <div class="containerr">
+
     <movie
-    v-for="movie in Movies"
-    :key="movie.id"
-    title="movie.title"
-    original="movie.original_title"
-    language="movie.original_language"
-    vote="movie.vote_average"
-     />
+    v-for="movie in movies"
+    :key="movie.id">
+    <h3>
+        {{movie.title}}
+    </h3>
+    <p class="orT">
+        {{movie.original_title}}
+    </p>
+    <p class="orL">
+        {{movie.original_language}}
+    </p>
+    <p class="Va">
+        {{movie.vote_average}}
+    </p>
+    </movie>
+    </div>
 </div>
 </div>
   
@@ -20,11 +31,11 @@ import axios from "axios";
 import Search from"./Search.vue"
 export default {
     components: {
-        Search
+        Search,
     },
   data() {
     return {
-      Movies: [],
+      movies: [],
       searchText:"",
     };
   },
@@ -32,9 +43,9 @@ export default {
     UserInput(text) {
         this.searchText = text
       axios
-        .get(`https://api.themoviedb.org/3/movie?api_key=2a36e3808dc852382706a41d635e3434e&query=${this.searchText}`)
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=3281705aad86751855ca2a484ec980fb&query=${this.searchText}`)
         .then((response) => {
-          this.Movies = response.data.results;
+          this.movies = response.data.results;
         });
     },
   },
@@ -48,5 +59,25 @@ export default {
 }
 ul{
     list-style: none;
+}
+.containerr{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+movie{
+    padding:1rem
+}
+h3{
+    color :white;
+}
+.orT, .orL, .Va{
+    font-weight: bold;
+}
+.orL{
+    color: greenyellow;
+}
+.Va{
+    color :red;
 }
 </style>
