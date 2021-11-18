9d745ba24906f89681ed5ca88ca4cc8edd7998a9
PaoloPascucci <pascuccip9@gmail.com>
2021-11-18 11:46:39 +0100

    re-create Vue folder and add main

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <siteMain/>
+  </div>
+</template>
+
+<script>
+import siteMain from "./components/siteMain.vue" ;
+
+export default {
+  name: 'App',
+  components: {
+    siteMain
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
