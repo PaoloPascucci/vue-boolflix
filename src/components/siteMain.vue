36009bcea11de6543bdb1c5f1200d200099522e7
PaoloPascucci <pascuccip9@gmail.com>
2021-11-18 12:05:42 +0100

    Add axios

--- src/components/siteMain.vue
+++ src/components/siteMain.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-      
+    
+      <button type="button">Submit</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+        data(){
+    return {
+      Movies: [],
+      }
+      },
+  mounted() {
+      setTimeout(this.Callapi, 1000)
+;
+  },
+  methods: {
 
-}
+   Callapi(){
+           axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        this.Movies = response.data.response;
+      })
+   },
+}}
 </script>
 
 <style>
-
+div{
+    height: 100vh;
+}
+button{
+    text-align: center;
+    width: 100px;
+    height: 100px;
+}
 </style>
